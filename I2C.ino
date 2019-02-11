7e865b3eed520e88e0f683433cd9c6ba626fdbec
martinaalien <47348338+martinaalien@users.noreply.github.com>
2019-02-11 09:40:11 +0100

    fiks

--- I2C.ino
+++ I2C.ino
@@ -7,6 +7,8 @@
 #define rtc_address  0x68
 #define MULTIGAS_ADDR_OLD     0x04        // default to 0x04
 #define MULTIGAS_ADDR_NEW     0x19        // change i2c address to 0x19
+
+
 uint16_t rtc_year;
 uint8_t rtc_month;
 uint8_t rtc_day;
@@ -19,11 +21,6 @@ typedef struct multigasreadings {
   float co;     //ppm
   float nh3;    //ppm
   float no2;    //ppm
-  float c3h8;   //ppm
-  float c4h10;  //ppm
-  float ch4;    //ppm
-  float h2;     //ppm
-  float c2h5oh; //ppm
 };
 
 multigasreadings gasread;
@@ -46,11 +43,6 @@ void multigas(){
   gasread.co = gas.measure_CO();
   gasread.nh3 = gas.measure_NH3();
   gasread.no2 = gas.measure_NO2();
-  gasread.c3h8 = gas.measure_C3H8();
-  gasread.c4h10 = gas.measure_C4H10();
-  gasread.ch4 = gas.measure_CH4();
-  gasread.h2 = gas.measure_H2();
-  gasread.c2h5oh = gas.measure_C2H5OH();
 }
 
 void rtc() {
